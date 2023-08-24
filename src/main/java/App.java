75d6707537c722d2741f516757ee82fcaf5fe3f3
Daunello <timurmusifullin@gmail.com>
2023-08-24 13:38:07 +0500

    Made task 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld FirstBean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld SecondBean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(FirstBean == SecondBean);
+
+        Cat FirstCat = (Cat) applicationContext.getBean("cat");
+        Cat SecondCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(FirstCat == SecondCat);
     }
 }
